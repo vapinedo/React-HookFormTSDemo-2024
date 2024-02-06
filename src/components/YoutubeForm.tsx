bb6ea4083f914113031e8ee9a4e96fc81aa1062d
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-02-06 11:30:49 -0500

    Managing Form State

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -2,25 +2,25 @@ import { useForm } from "react-hook-form"
 
 export default function YoutubeForm() {
 
-    const form = useForm();
-    
+    const { register } = useForm();
+
     return (
         <form>
             <h2 className="mb-4">Youtube Form</h2>
 
             <div className="mb-3">
                 <label className="form-label">Username</label>
-                <input type="text" className="form-control" name="username" />
+                <input type="text" className="form-control" {...register("username")} />
             </div>
 
             <div className="mb-3">
                 <label className="form-label">Email</label>
-                <input type="email" className="form-control" name="email" />
+                <input type="email" className="form-control" {...register("email")} />
             </div>
 
             <div className="mb-3">
                 <label className="form-label">Channel</label>
-                <input type="text" className="form-control" name="channel" />
+                <input type="text" className="form-control" {...register("channel")} />
             </div>
 
             <button className="btn btn-primary">Submit</button>

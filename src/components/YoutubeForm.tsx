3b7c685c868b54f890b3ef500b31660c0b1111d3
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-02-06 13:09:31 -0500

    Form Validation

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -15,17 +15,27 @@ export default function YoutubeForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
             <h2 className="mb-4">Youtube Form</h2>
 
             <div className="mb-3">
                 <label className="form-label">Username</label>
-                <input type="text" className="form-control" {...register("username")} />
+                <input type="text" className="form-control" {...register("username", {
+                    required: {
+                        value: true,
+                        message: "Username is required" 
+                    }
+                })} />
             </div>
 
             <div className="mb-3">
                 <label className="form-label">Email</label>
-                <input type="email" className="form-control" {...register("email")} />
+                <input type="email" className="form-control" {...register("email", {
+                    pattern: {
+                        value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                        message: "Invalid email format"
+                    }
+                })} />
             </div>
 
             <div className="mb-3">

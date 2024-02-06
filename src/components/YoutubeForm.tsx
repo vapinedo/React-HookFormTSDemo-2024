c7c40b7e252122c593862b1b5b0c917ab459e1a7
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-02-06 13:28:02 -0500

    Display Error Messages

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -8,7 +8,8 @@ type FormValues = {
 
 export default function YoutubeForm() {
 
-    const { register, handleSubmit } = useForm<FormValues>();
+    const { register, handleSubmit, formState } = useForm<FormValues>();
+    const { errors } = formState;
 
     function onSubmit(data: FormValues) {
         console.log("Form submitted", data);
@@ -26,21 +27,33 @@ export default function YoutubeForm() {
                         message: "Username is required" 
                     }
                 })} />
+                <small className="text-danger">{errors.username?.message}</small>
             </div>
 
             <div className="mb-3">
                 <label className="form-label">Email</label>
                 <input type="email" className="form-control" {...register("email", {
+                    required: {
+                        value: true,
+                        message: "Email is required"
+                    },
                     pattern: {
                         value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
                         message: "Invalid email format"
                     }
                 })} />
+                <small className="text-danger">{errors.channel?.message}</small>
             </div>
 
             <div className="mb-3">
                 <label className="form-label">Channel</label>
-                <input type="text" className="form-control" {...register("channel")} />
+                <input type="text" className="form-control" {...register("channel", {
+                    required: {
+                        value: true,
+                        message: "Channel is required"
+                    }
+                })} />
+                <small className="text-danger">{errors.channel?.message}</small>
             </div>
 
             <button className="btn btn-primary">Submit</button>
